de69c9d39f28f9dd6be07dafdc4a0f0b6c82bb96
Utkarsh Koppikar <47137566+utkarshkoppikar7@users.noreply.github.com>
2020-11-23 17:04:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+	var num1=0;
+	var num2=1;	
+	//document.getElementById('num1').innerHTML=num1;
+	//document.getElementById('num2').innerHTML=num2;
+	function clickHere(){
+		let cont=num2;
+		num2=num2+num1;
+		num1=cont;	
+		//document.getElementById('num1').innerHTML=num1;
+		//document.getElementById('num2').innerHTML=num2;
+		var para = document.createElement("p");
+		var node = document.createTextNode(num2);
+		para.appendChild(node);
+		var element = document.getElementById("div1");
+		element.appendChild(para);
+	}
