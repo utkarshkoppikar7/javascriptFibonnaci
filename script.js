6585cc050767195ae43544783c3e573682ad78f7
Utkarsh Koppikar <47137566+utkarshkoppikar7@users.noreply.github.com>
2020-11-23 20:28:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var num1=0;
-  var num2=1; 
+var num2=1; 
+var alertf=0,alert2=0;
   //document.getElementById('num1').innerHTML=num1;
   //document.getElementById('num2').innerHTML=num2;
   function clickHere(){
+    //var alertf=0;
     let cont=num2;
     num2=num2+num1;
     num1=cont;  
@@ -13,7 +15,13 @@ var num1=0;
     para.appendChild(node);
     var element = document.getElementById("div1");
     element.appendChild(para);
-    if(num2>100){
+    if(num2>100 && alertf==0){
       alert("Go on get a Life!!");
+      alertf=1;
+    }
+    if(num2>200 && alert2==0)
+    {
+      alert("Seriously get some help!!");
+      alert2=1;
     }
   }
